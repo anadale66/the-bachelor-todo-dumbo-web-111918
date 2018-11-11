48ef24ecce83902ed6e117b295627b29b2016b5d
Roman Pirag <mr.anadale@yahoo.com>
2018-11-11 23:26:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,32 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  
+  data.each do |seasons, contestant_info|
+    if seasons[season]
+      contestant_info.each do |contestant|
+        if contestant["status"] == "Winner"
+          return contestant["name"].split(" ")[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  return false
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  return false
 end
 
 def get_occupation(data, hometown)
   # code here
+  return false
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  return false
 end
