2eca8b9b9aed33ed7af9ef9e265709c872fdc3f9
Roman Pirag <mr.anadale@yahoo.com>
2018-11-12 00:31:30 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,7 +52,7 @@ def get_average_age_for_season(data, season)
         all_ages += contestant["age"].to_f
       end
     average = all_ages / contestant_info.length
-    return average.to_i
+    return average
     end
   end
 end
